517539462cf9c9ac6c589616a2f5ff1212945b0d
Computer <computer@Computers-MacBook-Pro.local>
2019-01-27 13:11:19 -0700

    added to users home page and selected user page. added to userjs show route and index route now goes to home page rather than profile page.

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const User = require('../models/users');
 const Workout = require('../models/workouts');
 
+//index route
+router.get('/', (req, res) => {
+    User.find({}, (err, allUsers) => {
+        res.render('users/home.ejs', {
+            users: allUsers
+        });
+    });
+});
 
 //create route
 router.get('/new', (req, res) => {
@@ -16,37 +24,38 @@ router.post('/', (req, res) => {
             res.send(err)
         } else {
             console.log(createdUser);
-            res.redirect('/profile')
+            res.redirect('/users')
         }
     });
 });
 
-//show for private page
+//show selecteduser page
 router.get('/:id', (req, res) => {
     User.findById(req.params.id, (err, foundUser) => {
         console.log(`foundUser ${foundUser}`)
         if (err) {
             res.send(err)
         } else {
-            res.render('users/show.ejs', {
+            res.render('users/selecteduser.ejs', {
                 user: foundUser
             });
         }
     })
 })
 
-//show public pages
-router.get('/:id', (req, res) => {
-    User.findById(req.params.id, (err, foundUser) => {
-        console.log(`foundUser ${foundUser}`)
-        if (err) {
-            res.send(err)
-        } else {
-            res.render('users/show.ejs', {
-                user: foundUser
-            });
-        }
-    })
-})
+// //show for private page
+// router.get('/:id', (req, res) => {
+//     User.findById(req.params.id, (err, foundUser) => {
+//         console.log(`foundUser ${foundUser}`)
+//         if (err) {
+//             res.send(err)
+//         } else {
+//             res.render('users/show.ejs', {
+//                 user: foundUser
+//             });
+//         }
+//     })
+// })
+
 
 module.exports = router;
